c730b37aff6faa9815d54b50a4bb742557e20d05
Mjte254 <ombogimaxi96@gmail.com>
2024-08-02 00:10:29 +0300

    0x0A-configuration_management

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,17 @@
-# install puppet-lint -v 2.5.0
+#install flask with puppet
 
-exec { 'puppet-lint':
-  command => '/usr/bin/apt-get -y install puppet-lint -v 2.5.0',
+package { 'python3.8':
+  ensure   => '3.8.10',
+  provider => 'pip3',
+}
+
+package { 'Flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
+}
+
+package { 'Werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip3',
+  require  => Package['Flask'],
 }
